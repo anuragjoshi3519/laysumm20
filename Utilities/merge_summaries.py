ffd368ed818aca6d5aa76c44789748fbd682493c
Ankush Khanna <ankushkhanna1998@gmail.com>
2020-10-12 16:27:11 +0530

    refactor: Utilities

--- Utilities/merge_summaries.py
+++ Utilities/merge_summaries.py
@@ -1,36 +1,34 @@
-import os
-import nltk
-nltk.download('punkt')
+from os import mkdir, listdir
+from os.path import isdir
 from nltk import sent_tokenize
 
-def mergeSumm():
-    
-    if not os.path.isdir("Data/Merged-final/bart_wMVC_ACD"):
-        os.mkdir("Data/Merged-final/bart_wMVC_ACD")
 
-    for file in os.listdir("Data/Section-wise-summaries/bart_Abstract"):
+def mergeSumm():
+    if not isdir('Data/Merged-final/bart_wMVC_ACD/'):
+        mkdir('Data/Merged-final/bart_wMVC_ACD/')
+
+    for file in listdir('Data/Section-wise-summaries/bart_Abstract/'):
         if not file.startswith('.'):
-            summary = ""
-            with open(f"Data/Section-wise-summaries/bart_Abstract/{file}","r") as f:
-                summary+=f.read()+'\n\n'
-            with open(f"Data/Section-wise-summaries/bart_Conclusions/{file}","r") as f:
+            summary = ''
+            with open(f'Data/Section-wise-summaries/bart_Abstract/{file}', 'r') as f:
+                summary += f.read() + '\n\n'
+            with open(f'Data/Section-wise-summaries/bart_Conclusions/{file}', 'r') as f:
                 init_text = f.read()
                 for sent in sent_tokenize(init_text):
-                    if len(summary.split())>100:
+                    if len(summary.split()) > 100:
                         break
-                    summary+=sent+'\n\n'    
-            with open(f"Data/Section-wise-summaries/bart_Discussion/{file}","r") as f:
+                    summary += sent + '\n\n'
+            with open(f'Data/Section-wise-summaries/bart_Discussion/{file}', 'r') as f:
                 init_text = f.read()
                 for sent in sent_tokenize(init_text):
-                    if len(summary.split())>100:
+                    if len(summary.split()) > 100:
                         break
-                    summary+=sent+'\n\n'  
-            with open(f"Data/Section-wise-summaries/bart_Introduction/{file}","r") as f:
+                    summary += sent + '\n\n'
+            with open(f'Data/Section-wise-summaries/bart_Introduction/{file}', 'r') as f:
                 init_text = f.read()
                 for sent in sent_tokenize(init_text):
-                    if len(summary.split())>100:
-                        break   
-                    summary+=sent+'\n\n'         
-            with open(f"Data/Merged-final/bart_wMVC_ACD/{file}","w") as f:
+                    if len(summary.split()) > 100:
+                        break
+                    summary += sent + '\n\n'
+            with open(f'Data/Merged-final/bart_wMVC_ACD/{file}', 'w') as f:
                 f.write(summary)
-            
